9e117e5b5a59973637be7dd4a8d8a6ef81ca42a3
huayratoro <49590235+huayratoro@users.noreply.github.com>
2019-12-13 20:12:01 -0300

    Update corre_WRF.sh
    ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ ACLARACIÓN !!!!!!!!!!!!!!!!!!! 
    Para correr este script hay que setear bien el environment e instalar cada dependencia correctamente...
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
    Los errores más comunes incluyen que las rutas estén mal seteadas, alguna librería mal instalada (especialmente si alguno de los test de compilación falló >> NI intentar correr un caso si esto ocurre !!! ) o bien están mal configuradas las namelist (WPS o input del WRF).
    
    Slds (HT)

--- corre_WRF.sh
+++ corre_WRF.sh
@@ -1,25 +1,30 @@
 #usr/bin/bash
-##################### script para correr el WRF desde el namelist.wps #####################
+##################### script para correr el WRF con algun caso real #####################
 ##################### POWERED BY HUAYRATORO #####################
+# Este bash solo corre con todas las librerias instaladas y los WPS, WRF y ARWpost compilados
+# para mas info sobre como setear el env e instalar librerias consultar alguno de estos enlaces :
+# https://www2.mmm.ucar.edu/wrf/OnLineTutorial/Introduction/start.php  >> del UCAR (con casos de estudio!)
+# https://www.youtube.com/watch?v=_9lBM4k7HQc >> video muy didactico !!
+# cambiar las direcciones y rutas a lo adecuado
 ##### IMPORTANTE : Recordar setear bien los namelist WPS, input y ARWpost porque sino tira error... 
 ## 	Primera parte: WPS
 # moverse hacia la carpeta WPS donde estan los tres ejecutables
-cd /home/marcos/Build_WRF/WPS/
+cd .../Build_WRF/WPS/
 # Ejecuto el geogrid.exe
 ./geogrid.exe
 ###### RECORDAR CREAR EL VTABLE PARA EL GFS O ECMWF EN EL DIRECTORIO ######
 ###### Sino crear con el comando : ln -sf ungrib/Variable_Tables/Vtable.GFS Vtable
 # Una vez creado el Vtable, entonces linkear los grib files, que pueden estar en una caprtea aparte del WPS 
-./link_grib.csh /home/marcos/Build_WRF/caso_ejemplo/analysis/gfsanl*
+./link_grib.csh .../Build_WRF/caso_ejemplo/analysis/gfsanl*
 # ahora ejecutar el ungrib.exe
 ./ungrib.exe
 # finalmente ejecutar el metgrid.exe 
 ./metgrib.exe
 ##	Segunda parte: WRF
 # moverse a la carpeta .../test/em_real de WRF
-cd /home/marcos/Build_WRF/WRF/test/em_real/
+cd .../Build_WRF/WRF/test/em_real/
 # linkear la salida de los metgrib a la carpeta WRF
-ln -sf /home/marcos/Build_WRF/WPS/met_em.d01* .
+ln -sf .../Build_WRF/WPS/met_em.d01* .
 ###### RECORDAR EDITAR EL NAMELIST.INPUT DE ACUERDO AL WPS PARA QUE NO TIRE ERROR
 # se corre el real.exe
 ./real.exe
@@ -28,7 +33,7 @@ ln -sf /home/marcos/Build_WRF/WPS/met_em.d01* .
 ##	Tercera parte: ARWpost
 # editar el namelist del ARWpost con las fechas y niveles que se quiera 
 # moverse a la carpeta del ARWpost
-cd /home/marcos/Build_WRF/ARWpost/
+cd .../Build_WRF/ARWpost/
 # se corre el arw.exe
 ./ARWpost.exe
 ###### listo ######
